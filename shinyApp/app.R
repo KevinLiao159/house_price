150ba60e0da2ba6c8faec073eb72ab23566d5452
Kevin Liao <lwk723@berkeley.edu>
2016-12-02 16:50:50 -0800

    modified file app.R : add title to the app

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -3,12 +3,12 @@ library(shiny)
 train <- read.csv("../data/rawData/train.csv")
 
 source("../script/function/util.R")
-num_items <- names(get_only_numerical_predictors(train))
+names_items <- names(get_only_numerical_predictors(train))
  
-train <- 
+numeric_items <- train[, names_items]
 
 ui <- fluidPage(
-    headerPanel('Simple Regression'),
+    headerPanel('Explanatory Data Analysis and Visualization'),
     sidebarPanel(
         selectInput('x', 'Input', names(advertising)[1:3])
     ),
