3032734a485276f2f95fe4aa52bb6ce13db5946a
Kevin Liao <lwk723@berkeley.edu>
2016-12-02 17:43:48 -0800

    modified file app.R: add histogram

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -1,17 +1,12 @@
 library(shiny)
 library(ggplot2)
 
-<<<<<<< HEAD
 train <- read.csv("../data/rawData/train.csv")
 
 source("../script/function/util.R")
 names_items <- names(get_only_numerical_predictors(train))
  
 numeric_items <- train[, names_items]
-=======
-test <- read.csv("../data/rawData/test.csv")
-advertising <- advertising[,2:5]
->>>>>>> 6b2256087f07511695d2057be39f9c07dcda4d64
 
 ui <- fluidPage(
     headerPanel('Explanatory Data Analysis and Visualization'),
@@ -19,8 +14,8 @@ ui <- fluidPage(
         selectInput('x', 'Input', names_items)
     ),
     mainPanel(
-        plotOutput('plot1')
-        
+        plotOutput('plot1') +
+        plotOutput('plot2')
     )
 )
 
@@ -32,13 +27,22 @@ server <- function(input, output) {
     })
     
     output$plot1 <- renderPlot({
-        ggplot(numeric_items, aes(x = numeric_items[,input$x], y = numeric_items$SalePrice)) + 
-          geom_point() +
-          ggtitle(paste0("Scatter Plot: Sale Price vs. ", input$x)) +
-          labs(x = input$x, y = "Sale Price")
-        
-
+      ggplot(numeric_items, aes(x = numeric_items[,input$x], y = numeric_items$SalePrice)) + 
+        geom_point() +
+        ggtitle(paste0("Scatter Plot: Sale Price vs. ", input$x)) +
+        labs(x = input$x, y = "Sale Price")
     })
+    
+    output$plot2 <- renderPlot({
+      ggplot(numeric_items, aes(x = numeric_items[,input$x])) +
+        geom_histogram() +
+        ggtitle(paste0("Histogram of ", input$x))
+      
+      
+    })
+    
+    
+    
 }
 
 shinyApp(ui = ui, server = server)
