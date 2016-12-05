a56ee59d6b3fcf999b0abaad4fbd038ee245908a
Kevin Liao <lwk723@berkeley.edu>
2016-12-04 20:01:49 -0800

    modified app.R: summary text content

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -28,9 +28,14 @@ model.list <- list(PCA = model.pca,
 
 ui <- fluidPage(
   
+  headerPanel('House Price: Advanced Regression Techniques'),
+  mainPanel(
+    h4("This Shiny App is designed in accordance with the report, which provides the structured outline of our project's methodology and analysis. The interactive App helps the reader to visualize the reproducible results and understand our appraoch to the problem.")
+  ),
+  
   headerPanel('Methodology'),
   mainPanel(
-    h4("The goal of this analysis is to accurately predict the final price of each home. Therefore, we frame this problem as a regression problem, and decide to use the L2 loss function [10] which is often used in regression problem. Taking this objective into account, we preprocess original dataset so that regression models can work well. Furthermore, we extract more features by involving feature engineering. Finally, we fit two shrinkage models and two ensemble models. The details are explained in the following subsections.")
+    h4("The goal of this analysis is to accurately predict the final price of each home. The methods we are using include the following steps: 1) Exploratory Data Analysis, which explores the initial relationship among variables; 2) Model Selection, which includes a variety of PCA, Lasso, Ridge, GBM and Random Forest; 3) Model Comparison, which aims to minimize RMSE of the prediction. The details are explained in the following subsections.")
   ),
   
 
@@ -51,7 +56,7 @@ ui <- fluidPage(
   headerPanel('Model Description'),
   
   mainPanel(
-    h4("While training each model, we need to find optimal paramters for each model. In order to effectively select hyper-parameters, we use 10-fold cross-validation. For lasso and ridge, lambda is the tuning parameter. It determines how much we will penalize models for high weights on predictors. If lambda is high, it penalizes models more and ends up generating sparse models. This kind of models is called shrinkage method because they shrinken weights or even remove predictors by penalizing models. These models are especially good options when there are many predictors. By penalziing or removing unnecessary predictors, they provide more interpretable results. Thus, they are often utilized in genomic and pharametical analysis. For PCR and PLSR, the number of principal components is the tuning parameter. They both internally use the precipal component analysis to obtain pricipal components, which are linear combination of original predictors in dataset. Based on spectral theorem, the eigenvector corresponding to highest eigenvalue is the direction that explains the most about the variability in data, which is the first principal component. We need to find the optimal number of subsets of PCs that summarize the entire data set without harming accuracy. Again, we use 10-kold cross validation to obtain the optimal number of principal components.")
+    h4("In order to effectively select hyper-parameters, we use 10-fold cross-validation. We first look at lasso and ridge, which are the shrinkage methods with lambda as the tuning parameter. We then look at the dimenson-reduction methods, including PCR and PLSR, where the number of principal components is the tuning parameter. We need to find the optimal number of subsets of PCs that summarize the entire data set without harming accuracy.")
   ),
   
   
