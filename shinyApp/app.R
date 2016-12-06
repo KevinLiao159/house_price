e214202368ab657b7b8496e5854ea6977289cef9
Kevin Liao <lwk723@berkeley.edu>
2016-12-05 20:25:23 -0800

    modified app.R: put text in panel

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -37,15 +37,22 @@ method.list <- list(lasso = model.lasso.df,
 ui <- fluidPage(
   
   headerPanel('House Price: Advanced Regression Techniques'),
-  mainPanel(
-    h4("This Shiny App is designed in accordance with the report, which provides the structured outline of our project's methodology and analysis. The interactive App helps the reader to visualize the reproducible results and understand our appraoch to the problem.")
-  ),
+  fluidRow(
+    column(12, offset = 0,
+           sidebarPanel(width = "100%", "This Shiny App is designed in accordance with the report, which provides the structured outline of our project's methodology and analysis. The interactive App helps the reader to visualize the reproducible results and understand our appraoch to the problem."
+           )
+           
+    )
+  ),  
   
   headerPanel('Methodology'),
-  mainPanel(
-    h4("The goal of this analysis is to accurately predict the final price of each home. The methods we are using include the following steps: 1) Exploratory Data Analysis, which explores the initial relationship among variables; 2) Model Selection, which includes a variety of PCA, Lasso, Ridge, GBM and Random Forest; 3) Model Comparison, which aims to minimize RMSE of the prediction. The details are explained in the following subsections.")
-  ),
-  
+  fluidRow(
+    column(12, offset = 0,
+           sidebarPanel(width = "100%", "The goal of this analysis is to accurately predict the final price of each home. The methods we are using include the following steps: 1) Exploratory Data Analysis, which explores the initial relationship among variables; 2) Model Selection, which includes a variety of PCA, Lasso, Ridge, GBM and Random Forest; 3) Model Comparison, which aims to minimize RMSE of the prediction. The details are explained in the following subsections."
+                        )
+    )
+  ),  
+    
 
   headerPanel('Explanatory Data Analysis and Visualization'),
   sidebarPanel(
@@ -59,9 +66,14 @@ ui <- fluidPage(
   
   
   headerPanel('Model Description'),
-  mainPanel(
-    h4("In order to effectively select hyper-parameters, we use 10-fold cross-validation. We first look at lasso and ridge, which are the shrinkage methods with lambda as the tuning parameter. We then look at the dimenson-reduction methods, including PCR and PLSR, where the number of principal components is the tuning parameter. We need to find the optimal number of subsets of PCs that summarize the entire data set without harming accuracy.")
-  ),
+  fluidRow(
+    column(12, offset = 0,
+           sidebarPanel(width = "100%", "In order to effectively select hyper-parameters, we use 10-fold cross-validation. We first look at lasso and ridge, which are the shrinkage methods with lambda as the tuning parameter. We then look at the dimenson-reduction methods, including PCR and PLSR, where the number of principal components is the tuning parameter. We need to find the optimal number of subsets of PCs that summarize the entire data set without harming accuracy."
+           )
+           
+    )
+  ),  
+
   
   
   headerPanel('Model Selection - Tunning Parameter'),
@@ -72,10 +84,13 @@ ui <- fluidPage(
   ),
   
   headerPanel('Model comparison'),
-  mainPanel(
-    h4("After fitting models with various methods, we use RMSE to compare models and select the best model. Although RMSE does not provide an absolute means of model accuracy, it provides a relative measure to compare models. Thus, we finalize our model with the lowest RMSE.")
-    ),
-    
+  fluidRow(
+    column(12, offset = 0,
+           sidebarPanel(width = "100%", "After fitting models with various methods, we use RMSE to compare models and select the best model. Although RMSE does not provide an absolute means of model accuracy, it provides a relative measure to compare models. Thus, we finalize our model with the lowest RMSE."
+           )
+    )
+  ),  
+  
   
   headerPanel('Results'),
   sidebarPanel(
@@ -92,11 +107,6 @@ ui <- fluidPage(
     plotOutput('plot5'),
     plotOutput('plot6')
   )
-  
-  
-  
-  
-  
 )
   
 
