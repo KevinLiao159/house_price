8f91ad93e9122c61e9d1e326ef5f7d4c4ebedec1
Kevin Liao <lwk723@berkeley.edu>
2016-12-05 22:24:31 -0800

    complete test_get_number_none.R

--- code/tests/test_get_number_none.R
+++ code/tests/test_get_number_none.R
@@ -7,10 +7,11 @@ context("Test for get_number_none() function")
 
 test_that("get_number_none() works as expected", {
   
-  vec <- c(1, 1, 1, "None", 1, "None", 1, 1)
+  df <- data.frame(a = c(1, 1, 1, "None", 1, "None", 1, 1), 
+                   b= c(1, 1, 1, "None", 1, "None", 1, 1))
   
-  expect_equal(mse(fitted_value, true_value), mean((fitted_value - true_value)^2))
-  expect_type(mse(fitted_value, true_value), 'double')
-  expect_length(mse(fitted_value, true_value), 1)
-})
+  expect_equal(get_number_none(df), c(0, 0, 0, 2, 0, 2, 0, 0))
+  expect_type(get_number_none(df), 'integer')
+  expect_length(get_number_none(df), 8)
+}) 
 
